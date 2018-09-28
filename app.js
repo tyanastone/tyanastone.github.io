e92b48116c6933ee69cfab88b23fae5fd0b02424
Tyana <tyana.stone@gmail.com>
2018-09-27 21:06:24 -0400

    finished project

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-window.addEventListener("load", function(){
-    var node = document.querySelector("[data-loading]");
-    node.setAttribute("data-loading","complete");
-});
+// window.addEventListener("load", function(){
+//     var node = document.querySelector("[data-loading]");
+//     node.setAttribute("data-loading","complete");
+// });
 window.addEventListener("scroll", function(){
     var speed = 5;
     var size = 100+(window.scrollY/speed);
@@ -9,18 +9,18 @@ window.addEventListener("scroll", function(){
     style.backgroundSize = size+"% "+size+"%";
 });
 
-window.addEventListener("load", function(){
-	var nodes = document.querySelectorAll(".ripple");
-	for(var i=0; i<nodes.length; i++){
-		var letters = nodes[i].innerText.split('').join('</span><span>');
-		letters = letters.split(' ').join('&nbsp;');
-		nodes[i].innerHTML = "<span>"+letters+"</span>";
+// window.addEventListener("load", function(){
+// 	var nodes = document.querySelectorAll(".ripple");
+// 	for(var i=0; i<nodes.length; i++){
+// 		var letters = nodes[i].innerText.split('').join('</span><span>');
+// 		letters = letters.split(' ').join('&nbsp;');
+// 		nodes[i].innerHTML = "<span>"+letters+"</span>";
 
-		var children = nodes[i].childNodes;
-		for(var n=0; n<children.length; n++){
-			children[n].style.animationDelay = (n/10)+"s";
-		}
+// 		var children = nodes[i].childNodes;
+// 		for(var n=0; n<children.length; n++){
+// 			children[n].style.animationDelay = (n/10)+"s";
+// 		}
 
-	}
+// 	}
 
-});
+// });
